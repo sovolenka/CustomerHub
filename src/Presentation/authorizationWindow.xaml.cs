cba9dd5727f5659987d94dae74421efcf0b65db3
Adamantly <pavlo@DESKTOP-JL8LL60>
2023-11-12 21:12:33 +0200

    restyle code

--- src/Presentation/authorizationWindow.xaml.cs
+++ src/Presentation/authorizationWindow.xaml.cs
@@ -1,53 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
-namespace Presentation
+namespace Presentation;
+
+public partial class AuthorizationWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for authorizationWindow.xaml
-    /// </summary>
-    public partial class authorizationWindow : Window
+
+    private void logAccount(object sender, RoutedEventArgs e)
     {
 
-        private void ButtonClick(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void ForgotPasswordTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            // Логіка для обробки натискання тексту "Забули пароль"
-        }
-
-        private void RegisterTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            // Логіка для обробки натискання тексту "Зареєструватися"
-        }
-
-        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
-        {
-            // Логіка для обробки отримання фокусу на полі введення тексту
-        }
-
-        private void PasswordBox_GotFocus(object sender, RoutedEventArgs e)
-        {
-            // Логіка для обробки отримання фокусу на полі введення паролю
-        }
-            
-        public authorizationWindow()
-        {
-            InitializeComponent();
-        }
     }
+
+    private void forgotPasswordTextBlockMouseDown(object sender, MouseButtonEventArgs e)
+    {
+        // Логіка для обробки натискання тексту "Забули пароль"
+    }
+
+    private void registerTextBlockMouseDown(object sender, MouseButtonEventArgs e)
+    {
+        // Логіка для обробки натискання тексту "Зареєструватися"
+    }
+
+    private void textBoxGotFocus(object sender, RoutedEventArgs e)
+    {
+        // Логіка для обробки отримання фокусу на полі введення тексту
+    }
+
+    private void passwordBoxGotFocus(object sender, RoutedEventArgs e)
+    {
+        // Логіка для обробки отримання фокусу на полі введення паролю
+    }
+        
+    public AuthorizationWindow()
+    {
+        InitializeComponent();
+    }
+
 }
--- src/Presentation/registrationWindow.xaml.cs
+++ src/Presentation/registrationWindow.xaml.cs
@@ -1,28 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
-namespace Presentation
+namespace Presentation;
+
+public partial class RegistrationWindow : Page
 {
-    /// <summary>
-    /// Interaction logic for registrationWindow.xaml
-    /// </summary>
-    public partial class registrationWindow : Page
+    public RegistrationWindow()
     {
-        public registrationWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
