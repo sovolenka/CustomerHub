f10accc9c0eecb7b5c01d2286b6ae52d54d6548e
Adamantly <pavlo@DESKTOP-JL8LL60>
2023-11-12 16:01:13 +0200

    New 2 windows

--- src/Presentation/registrationWindow.xaml.cs
+++ src/Presentation/registrationWindow.xaml.cs
@@ -16,11 +16,11 @@ using System.Windows.Shapes;
 namespace Presentation
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for registrationWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class registrationWindow : Page
     {
-        public MainWindow()
+        public registrationWindow()
         {
             InitializeComponent();
         }
