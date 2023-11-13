7a0ed58175b8fb2afb0bb5c42e1fe2089ea9ba34
Olenka Stoliar <olenka04st@gmail.com>
2023-11-13 13:48:17 +0200

    Add `SQLiteContext` and `SQLiteContextSingleton`

---
+++ src/Data/Context/SQLiteContext.cs
@@ -0,0 +1,28 @@
+﻿using Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Context;
+
+public class SQLiteContext: DbContext
+{
+    public DbSet<User>? Users { get; set; }
+    public DbSet<Client>? Clients { get; set; }
+    public DbSet<Product>? Products { get; set; }
+
+    public string DbPath { get; }
+
+    internal SQLiteContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = Path.Join(path, "customerhub.db");
+        Database.EnsureCreated();
+    }
+
+    // The following configures EF to create a Sqlite database file in the
+    // special "local" folder for your platform.
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlite($"Data Source={DbPath}");
+    }
+}
