8a7a0928406ae7e37c9e2abe2a2ef140694d84ee
KhrystynaBat <144723266+KhrystynaBat@users.noreply.github.com>
2023-11-20 04:02:32 +0200

    -

--- src/Presentation/ProgramWindow.xaml.cs
+++ src/Presentation/ProgramWindow.xaml.cs
@@ -11,29 +11,51 @@ namespace Presentation
             InitializeComponent();
         }
 
+        private void openEditAccountWindow(object sender, RoutedEventArgs e)
+        {
+            EditAccountWindow editAccountWindow = new EditAccountWindow();
+            editAccountWindow.Show();
+        }
+
+        private void logOutClick(object sender, RoutedEventArgs e)
+        {
+            AuthorizationWindow authorizationWindow = new AuthorizationWindow();
+            authorizationWindow.Show();
+            this.Close();
+        }
+
         private void importClick(object sender, RoutedEventArgs e)
         {
-
+            /*
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filePath = openFileDialog.FileName;
+                // Read the file and process it
+                string fileContent = File.ReadAllText(filePath);
+                // Add your logic to handle the file content
+            }
+            */
         }
+
 
         private void exportClick(object sender, RoutedEventArgs e)
         {
-
+            /*
+            Microsoft.Win32.SaveFileDialog saveFileDialog = new Microsoft.Win32.SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                string filePath = saveFileDialog.FileName;
+                // Add your logic to get the data you want to export
+                string dataToExport = "Your data to export"; // Replace this with your actual data
+                File.WriteAllText(filePath, dataToExport);
+            }
+            */
         }
 
-        private void clientListClick(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void productListClick(object sender, RoutedEventArgs e)
-        {
-
-        }
 
         private void openClientListWindow(object sender, RoutedEventArgs e)
         {
-
             ClientListWindow clientListWindow = new ClientListWindow();
             clientListWindow.Show();
         }
