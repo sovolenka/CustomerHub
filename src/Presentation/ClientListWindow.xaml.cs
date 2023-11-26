c63398d04d4f93c1116a6fa8b4c232067092d4c9
Olenka <olenka04st@gmail.com>
2023-11-26 22:11:09 +0200

    Implement `ClientListWindow`

--- src/Presentation/ClientListWindow.xaml.cs
+++ src/Presentation/ClientListWindow.xaml.cs
@@ -1,31 +1,71 @@
-﻿using System.Windows;
+﻿using System;
+using System.Linq;
+using System.Windows;
+using Business.Services;
+using Data.Models;
+using Presentation.Events;
 
 
 namespace Presentation
 {
     public partial class ClientListWindow : Window
     {
+        private readonly ClientService _clientService;
+
+        // Filter predicate
+        private Predicate<Client>? _filterPredicate;
+
         public ClientListWindow()
         {
+            _clientService = new ClientService();
             InitializeComponent();
+            ClientList.ItemsSource = _clientService.GetAll(AuthorizationService.AuthorizedUser!);
         }
 
         private void OpenAddClientWindow(object sender, RoutedEventArgs e)
         {
             AddClientWindow addClientWindow = new AddClientWindow();
-
+            addClientWindow.ClientAdded += OnClientsUpdate;
             addClientWindow.Show();
+        }
+
+        // Event handler for the ClientAdded event
+        private void OnClientsUpdate(object? sender, ClientEventArgs e)
+        {
+            // Refresh or update your ClientList here
+            // You may need to retrieve the updated data from the database
+            ClientList.ItemsSource = _filterPredicate is null
+                ? _clientService.GetAll(AuthorizationService.AuthorizedUser!)
+                : _clientService.GetAll(AuthorizationService.AuthorizedUser!)
+                    .Where(client => _filterPredicate(client));
+        }
+
+        private void OnPredicateUpdate(object? sender, ClientPredicateEventArgs e)
+        {
+            _filterPredicate = e.Predicate;
+            ClientList.ItemsSource = _clientService.GetAll(AuthorizationService.AuthorizedUser!)
+                .Where(client => _filterPredicate(client));
         }
 
         private void OpenEditClientWindow(object sender, RoutedEventArgs e)
         {
-            EditClientWindow editClientWindow = new EditClientWindow();
-
-            editClientWindow.Show();
+            Client selectedClient = (Client)ClientList.SelectedItem;
+            UpdateClientWindow updateClientWindow = new UpdateClientWindow(selectedClient);
+            updateClientWindow.ClientAdded += OnClientsUpdate;
+            updateClientWindow.Show();
         }
 
         private void DeleteClientClick(object sender, RoutedEventArgs e)
         {
+            Client selectedClient = (Client)ClientList.SelectedItem;
+            
+            MessageBoxResult messageBoxResult = MessageBox.Show(
+                $"Ви впевнені, що хочете видалити {selectedClient.FirstName} {selectedClient.SecondName}?",
+                "Delete Confirmation", MessageBoxButton.YesNo);
+            if (messageBoxResult == MessageBoxResult.No) return;
+            
+            _clientService.Remove(selectedClient);
+            ClientList.ItemsSource = _clientService.GetAll(AuthorizationService.AuthorizedUser!);
         }
 
         private void FilterClientClick(object sender, RoutedEventArgs e)
