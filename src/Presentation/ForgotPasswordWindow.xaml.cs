2964a418e1eacc6f708e9c94f7f713c8b5b5ea31
Olenka <olenka04st@gmail.com>
2023-11-26 22:12:38 +0200

    Refactor

---
+++ src/Presentation/ForgotPasswordWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Presentation;
+
+public partial class ForgotPasswordWindow : Window
+{
+    public ForgotPasswordWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void ImageMouseDown(object sender, MouseButtonEventArgs e)
+    {
+        AuthorizationWindow authorizationWindow = new AuthorizationWindow();
+        Hide();
+        authorizationWindow.ShowDialog();
+        Close();
+    }
+
+    private void SendEmailButton(object sender, MouseButtonEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+}
