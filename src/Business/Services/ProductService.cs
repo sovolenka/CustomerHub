014e0ef4aa8eee1b0569e33427f00e8f6cdd9263
Adamant1y <124239912+Adamant1y@users.noreply.github.com>
2023-12-18 22:10:26 +0200

    Add StyleCop and fix warnings

--- src/Business/Services/ProductService.cs
+++ src/Business/Services/ProductService.cs
@@ -1,74 +1,75 @@
-﻿using Data.Context;
-using Data.Models;
-using Microsoft.EntityFrameworkCore;
-
-namespace Business.Services;
-
-public class ProductService
+﻿namespace Business.Services
 {
-    private readonly SQLiteContext _context = SQLiteContextSingleton.Instance;
+    using Data.Context;
+    using Data.Models;
+    using Microsoft.EntityFrameworkCore;
+    public class ProductService
+    {
+        private readonly SQLiteContext _context = SQLiteContextSingleton.Instance;
 
-    public ProductService(SQLiteContext context)
-    {
-        _context = context;
-    }
-    
-    public ProductService()
-    {
-        _context = SQLiteContextSingleton.Instance;
-    }
-    
-    public Product? Add(Product product, User? user = null)
-    {
-        if (user is not null) product.User = user;
-        Product? added = _context.Products?.Add(product).Entity;
-        _context.SaveChanges();
-        return added;
-    }
-
-    public Product? Remove(Product product)
-    {
-        Product? removed = _context.Products?.Remove(product).Entity;
-        _context.SaveChanges();
-        return removed;
-    }
-
-    public Product? Update(Product product)
-    {
-        Product? updated = _context.Products?.Update(product).Entity;
-        _context.SaveChanges();
-        return updated;
-    }
-
-    public IEnumerable<Product> GetAllByUser(User user)
-    {
-        return _context.Products!
-            .Include(p => p.Characteristic)
-            .Where(p => p.User == user).ToList();
-    }
-
-    public static bool ProductContains(Product product, string query)
-    {
-        query = query.ToLower();
-        return product.Name!.ToLower().Contains(query) ||
-               product.Price.ToString().Contains(query) ||
-               product.Characteristic!.ProductType!.ToLower().Contains(query) ||
-               product.Characteristic!.Category!.ToLower().Contains(query) ||
-               product.Characteristic!.Description!.ToLower().Contains(query) ||
-               product.Characteristic!.Manufacturer!.ToLower().Contains(query) ||
-               product.Characteristic!.Country!.ToLower().Contains(query) ||
-               product.Characteristic.ManufactureDate.ToString().ToLower().Contains(query) ||
-               product.Characteristic.Status!.ToString().ToLower().Contains(query);
-    }
-
-    public bool IsProductNameUnique(string? productName, User user)
-    {
-        if (productName == null)
+        public ProductService(SQLiteContext context)
         {
-            return false;
+            _context = context;
         }
-        IEnumerable<Product> existingProducts = GetAllByUser(user);
-        return !existingProducts.Any(product =>
-            product.Name != null && product.Name.Equals(productName, StringComparison.OrdinalIgnoreCase));
+
+        public ProductService()
+        {
+            _context = SQLiteContextSingleton.Instance;
+        }
+
+        public Product? Add(Product product, User? user = null)
+        {
+            if (user is not null) product.User = user;
+            Product? added = _context.Products?.Add(product).Entity;
+            _context.SaveChanges();
+            return added;
+        }
+
+        public Product? Remove(Product product)
+        {
+            Product? removed = _context.Products?.Remove(product).Entity;
+            _context.SaveChanges();
+            return removed;
+        }
+
+        public Product? Update(Product product)
+        {
+            Product? updated = _context.Products?.Update(product).Entity;
+            _context.SaveChanges();
+            return updated;
+        }
+
+        public IEnumerable<Product> GetAllByUser(User user)
+        {
+            return _context.Products!
+                .Include(p => p.Characteristic)
+                .Where(p => p.User == user).ToList();
+        }
+
+        public static bool ProductContains(Product product, string query)
+        {
+            query = query.ToLower();
+            return product.Name!.ToLower().Contains(query) ||
+                   product.Price.ToString().Contains(query) ||
+                   product.Characteristic!.ProductType!.ToLower().Contains(query) ||
+                   product.Characteristic!.Category!.ToLower().Contains(query) ||
+                   product.Characteristic!.Description!.ToLower().Contains(query) ||
+                   product.Characteristic!.Manufacturer!.ToLower().Contains(query) ||
+                   product.Characteristic!.Country!.ToLower().Contains(query) ||
+                   product.Characteristic.ManufactureDate.ToString().ToLower().Contains(query) ||
+                   product.Characteristic.Status!.ToString().ToLower().Contains(query);
+        }
+
+        public bool IsProductNameUnique(string? productName, User user)
+        {
+            if (productName == null)
+            {
+                return false;
+            }
+            IEnumerable<Product> existingProducts = GetAllByUser(user);
+            return !existingProducts.Any(product =>
+                product.Name != null && product.Name.Equals(productName, StringComparison.OrdinalIgnoreCase));
+        }
     }
 }
+
--- src/Business/Services/UserService.cs
+++ src/Business/Services/UserService.cs
@@ -1,57 +1,60 @@
-﻿using Data.Context;
-using Data.Models;
+﻿
 
-namespace Business.Services;
-
-public class UserService
+namespace Business.Services
 {
-    private readonly SQLiteContext _context;
+    using Data.Context;
+    using Data.Models;
+    public class UserService
+    {
+        private readonly SQLiteContext _context;
 
-    public UserService(SQLiteContext context)
-    {
-        _context = context;
-    }
-    
-    public UserService()
-    {
-        _context = SQLiteContextSingleton.Instance;
-    }
-    
-    public User? Get(int id)
-    {
-        return _context.Users!.Find(id);
-    }
-    
-    public User? Add(User user)
-    {
-        if (ContainsEmail(user.Email!)) return null;
-
-        User? added = _context.Users?.Add(user).Entity;
-        _context.SaveChanges();
-        return added;
-    }
-
-    public bool ContainsEmail(string email)
-    {
-        foreach (var u in _context.Users!)
+        public UserService(SQLiteContext context)
         {
-            if (u.Email == email) return true;
+            _context = context;
         }
 
-        return false;
-    }
+        public UserService()
+        {
+            _context = SQLiteContextSingleton.Instance;
+        }
 
-    public User? Remove(User user)
-    {
-        User? removed = _context.Users?.Remove(user).Entity;
-        _context.SaveChanges();
-        return removed;
-    }
+        public User? Get(int id)
+        {
+            return _context.Users!.Find(id);
+        }
 
-    public User? Update(User user)
-    {
-        User? updated = _context.Users?.Update(user).Entity;
-        _context.SaveChanges();
-        return updated;
+        public User? Add(User user)
+        {
+            if (ContainsEmail(user.Email!)) return null;
+
+            User? added = _context.Users?.Add(user).Entity;
+            _context.SaveChanges();
+            return added;
+        }
+
+        public bool ContainsEmail(string email)
+        {
+            foreach (var u in _context.Users!)
+            {
+                if (u.Email == email) return true;
+            }
+
+            return false;
+        }
+
+        public User? Remove(User user)
+        {
+            User? removed = _context.Users?.Remove(user).Entity;
+            _context.SaveChanges();
+            return removed;
+        }
+
+        public User? Update(User user)
+        {
+            User? updated = _context.Users?.Update(user).Entity;
+            _context.SaveChanges();
+            return updated;
+        }
     }
 }
+
